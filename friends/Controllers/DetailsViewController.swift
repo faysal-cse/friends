0d2aaa30a9887bf14ec2579a6d9ff9636d2e1e6c
Faysal <faysalcse08@gmail.com>
2023-03-30 10:52:00 +0600

    remove unnecessary code

--- friends/Controllers/DetailsViewController.swift
+++ friends/Controllers/DetailsViewController.swift
@@ -34,35 +34,10 @@ class DetailsViewController: UIViewController {
         }
 
     }
-    
-    func showSimpleActionSheet(controller: UIViewController) {
-            let alert = UIAlertController(title: "Title", message: "Please Select an Option", preferredStyle: .actionSheet)
-            alert.addAction(UIAlertAction(title: "Approve", style: .default, handler: { (_) in
-                print("User click Approve button")
-            }))
-
-            alert.addAction(UIAlertAction(title: "Edit", style: .default, handler: { (_) in
-                print("User click Edit button")
-            }))
-
-            alert.addAction(UIAlertAction(title: "Delete", style: .destructive, handler: { (_) in
-                print("User click Delete button")
-            }))
-
-            alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: { (_) in
-                print("User click Dismiss button")
-            }))
-
-            self.present(alert, animated: true, completion: {
-                print("completion block")
-            })
-        }
-    
+  
     
     @IBAction func emailBtnAction(_ sender: UIButton) {
-        showSimpleActionSheet(controller: self)
-        
-        
+                
         if MFMailComposeViewController.canSendMail() {
             let mail = MFMailComposeViewController()
             mail.mailComposeDelegate = self
